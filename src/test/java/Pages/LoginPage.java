43ae8647119d42e841f4c477803c27df92abdf3b
JamesRK5 <nogamesjamesrealestate@gmail.com>
2023-04-21 23:38:58 -0400

    Added Login Page, Page Initializer and driver into the common methods.

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,4 +1,27 @@
 package Pages;
 
-public class LoginPage {
-}
+import Utils.CommonMethods;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends CommonMethods {
+
+ public LoginPage() {
+     PageFactory.initElements(driver, this);}
+
+     @FindBy(id= "txtUsername")
+     public WebElement UsernameTxtbox;
+
+     @FindBy(id = "txtPassword")
+     public WebElement UsernamePswdBox;
+
+     @FindBy(id = "btnLogin")
+     public WebElement LoginBtn;
+
+    @FindBy(id="spanMessage")
+    public WebElement InvalidLogin;
+
+
+ }
+
--- src/test/java/Utils/CommonMethods.java
+++ src/test/java/Utils/CommonMethods.java
@@ -1,4 +1,10 @@
 package Utils;
 
-public class CommonMethods {
+import StepDefinitions.PageInitializer;
+import org.openqa.selenium.WebDriver;
+
+public class CommonMethods extends PageInitializer {
+
+    public static WebDriver driver;
+
 }
