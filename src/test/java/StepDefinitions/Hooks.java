9c58606dc404ac8a8c4ed17e7b83f71b75becfd0
Grant <grantqfaulkner@gmail.com>
2023-04-23 22:35:52 -0400

    Merged Admin Login and added Hooks

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -1,9 +1,32 @@
 package StepDefinitions;
 
 import Utils.CommonMethods;
-import org.junit.After;
-import org.junit.Before;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 public class Hooks extends CommonMethods {
 
+    @Before
+    public void preCondition(){
+        openBrowserAndLaunchApplication();
+    }
+    @After
+    public void postConditions(Scenario scenario)
+    {
+        byte[] pic;
+        if(scenario.isFailed()){
+            pic=takeScreenshot("failed/" +scenario.getName());
+        }else {
+            pic=takeScreenshot("passwed/"+scenario.getName());
+        }
+
+        //attaches screenshot to reports
+        scenario.attach (pic,"image/png", scenario.getName());
+
+
+        closeBrowser();
+
+    }
 }
+
